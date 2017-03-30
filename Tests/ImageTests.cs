fc3fe0bfc9781281fe31a21cc9a200a3f134ce53
boginw <bogi.wennerstrom@gmail.com>
2017-03-30 14:52:28 +0200

    Test implementation

---
+++ Tests/ImageTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using SiftSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiftSharp.Tests
+{
+    [TestFixture()]
+    public class ImageTests
+    {
+        [Test()]
+        public void readImageTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
