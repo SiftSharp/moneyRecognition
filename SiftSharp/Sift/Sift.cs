b85a6f687db205a323ed69245db8e5e4c9b08de3
msbo <mbojer16@student.aau.dk>
2017-04-02 14:46:03 +0200

    Fix levels and var

--- SiftSharp/Sift/Sift.cs
+++ SiftSharp/Sift/Sift.cs
@@ -1,18 +1,18 @@
 ﻿using System.Linq;
+using System;
 
 namespace SiftSharp.Sift
 {
     public class Sift
     {
-        private int intervalsInOctave;
+        private int levelsInOctave;
         private int numberOfOctaves;
 
-        public Sift(int intervalsInOctave, int numberOfOctaves)
+        public Sift(int levelsInOctave, int numberOfOctaves)
         {
-            this.intervalsInOctave = intervalsInOctave;
+            this.levelsInOctave = levelsInOctave;
             this.numberOfOctaves = numberOfOctaves;
         }
-
 
         /// <summary>
         /// Creates DoG in Scale Space from Gaussian Pyramid in Scale Space
@@ -25,7 +25,7 @@ namespace SiftSharp.Sift
             int gaussHeight = gaussPyramid[0][0].GetLength(1);
 
             int[][,] imagesInOctaves = Enumerable
-                .Range(0, intervalsInOctave - 1)
+                .Range(0, levelsInOctave - 1)
                 .Select(_ => new int[gaussWidth, gaussHeight])
                 .ToArray();
 
@@ -37,18 +37,18 @@ namespace SiftSharp.Sift
             //For each octave
             for (int octave = 0; octave < numberOfOctaves; octave++)
             {
-                //For each picture in each octave
-                for (int img = 1; img < intervalsInOctave; img++)
+                //For each picture in each octave (from 1 to +1 because of 2 extra layers (s+3 in gaussPyr))
+                for (int level = 1; level < levelsInOctave + 1; level++)
                 {
                     //For each y in the image
-                    for (int y = 0; y < gaussPyramid[octave][img].GetLength(1); y++)
+                    for (int y = 0; y < gaussPyramid[octave][level].GetLength(1); y++)
                     {
                         //For each x in the image
-                        for (int x = 0; x < gaussPyramid[octave][img].GetLength(0); x++)
+                        for (int x = 0; x < gaussPyramid[octave][level].GetLength(0); x++)
                         {
                             //Subtract each pixel x,y in current image indexed from one with previous for each level
-                            result[octave][img-1][x, y] =
-                                gaussPyramid[octave][img][x, y] - gaussPyramid[octave][img - 1][x, y];
+                            result[octave][level-1][x, y] =
+                                gaussPyramid[octave][level][x, y] - gaussPyramid[octave][level - 1][x, y];
                         }
                     }
                 }
