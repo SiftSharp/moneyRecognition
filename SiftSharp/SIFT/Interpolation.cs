050514474f532eaca2386845c73296b5328fc0d5
NatalieMark <natalies2009@hotmail.com>
2017-04-06 00:40:17 +0200

    dogPyr -> dogPyramid

--- SiftSharp/SIFT/Interpolation.cs
+++ SiftSharp/SIFT/Interpolation.cs
@@ -9,7 +9,7 @@ namespace SiftSharp.SIFT
         /// <summary>
         /// Checks if extremum opholds criteria
         /// </summary>
-        /// <param name="dogPyr">Difference-of-Gaussian pyramid</param>
+        /// <param name="dogPyramid">Difference-of-Gaussian pyramid</param>
         /// <param name="octave">Octave to look in</param>
         /// <param name="level">Level to look in</param>
         /// <param name="x">X coordinate</param>
@@ -17,14 +17,14 @@ namespace SiftSharp.SIFT
         /// <param name="numberOfLevels">Number of levels per octave</param>
         /// <param name="curvatureTresh">Curvature threshold</param>
         /// <returns>Feature or null</returns>
-        public static Feature InterpolatesExtremum(Image[][] dogPyr, int octave, int level, int x,
+        public static Feature InterpolatesExtremum(Image[][] dogPyramid, int octave, int level, int x,
             int y, int numberOfLevels, float curvatureTresh)
         {
             double xLevel = 0, xRow = 0, xCol = 0;
             int maxSteps = 5;
             for (int i = 0; i < maxSteps; i++)
             {
-                InterpolationStep(dogPyr, octave, level, x, y, out xLevel, out xRow, out xCol);
+                InterpolationStep(dogPyramid, octave, level, x, y, out xLevel, out xRow, out xCol);
                 if (Math.Abs(xLevel) < 0.5 && Math.Abs(xRow) < 0.5 && Math.Abs(xCol) < 0.5)
                 {
                     break;
@@ -35,14 +35,14 @@ namespace SiftSharp.SIFT
                 level += (int)Math.Round(xLevel);
 
                 if (level < 1 || level > numberOfLevels - 2 || x < 1 || y < 1 || 
-                    x >= dogPyr[octave][0].Get().GetLength(0) - 1 ||
-                    y >= dogPyr[octave][0].Get().GetLength(1) - 1)
+                    x >= dogPyramid[octave][0].Get().GetLength(0) - 1 ||
+                    y >= dogPyramid[octave][0].Get().GetLength(1) - 1)
                 {
                     return null;
                 }
             }
             
-            double contrast = InterpolatedPixelContrast(dogPyr, octave, level, x, y, xLevel, xRow, xCol);
+            double contrast = InterpolatedPixelContrast(dogPyramid, octave, level, x, y, xLevel, xRow, xCol);
             if (Math.Abs(contrast) < curvatureTresh / numberOfLevels)
             {
                 return null;
@@ -61,7 +61,7 @@ namespace SiftSharp.SIFT
         /// <summary>
         /// Calculates interpolated contrast. Based on Equation 3 from Lowe's paper.
         /// </summary>
-        /// <param name="dogPyr" type="Image[][]">Difference-of-Gaussian pyramid</param>
+        /// <param name="dogPyramid" type="Image[][]">Difference-of-Gaussian pyramid</param>
         /// <param name="octave">Octaves in pyramid</param>
         /// <param name="level">Levels per octave</param>
         /// <param name="x">Pixel X coordinate</param>
@@ -70,19 +70,19 @@ namespace SiftSharp.SIFT
         /// <param name="xRow">Interpolated subpixel increment to row</param>
         /// <param name="xCol">Interpolated subpixel increment to col</param>
         /// <returns></returns>
-        public static double InterpolatedPixelContrast(Image[][] dogPyr, int octave, int
+        public static double InterpolatedPixelContrast(Image[][] dogPyramid, int octave, int
             level, int x, int y, double xLevel, double xRow, double xCol)
         {
             Vector<double> intrVector = new DenseVector(new double[] { xCol, xRow, xLevel });
-            Matrix<double> derivative = Derivative3D(dogPyr, octave, level, x, y).ToColumnMatrix();
+            Matrix<double> derivative = Derivative3D(dogPyramid, octave, level, x, y).ToColumnMatrix();
             Vector<double> intrContrast = derivative.Transpose().Multiply(intrVector);
-            return dogPyr[octave][level].Get()[x, y] + intrContrast[0] * 0.5;
+            return dogPyramid[octave][level].Get()[x, y] + intrContrast[0] * 0.5;
         }
 
         /// <summary>
         /// Performs one step of extremum interpolation. Equation 3 from Lowe's paper.
         /// </summary>
-        /// <param name="dogPyr" type="Image[][]">Difference-of-Gaussian pyramid</param>
+        /// <param name="dogPyramid" type="Image[][]">Difference-of-Gaussian pyramid</param>
         /// <param name="octave">Octaves in pyramid</param>
         /// <param name="level">Levels per octave</param>
         /// <param name="x">X coordinate</param>
@@ -90,11 +90,11 @@ namespace SiftSharp.SIFT
         /// <param name="xLevel">Output param</param>
         /// <param name="xRow">Output param</param>
         /// <param name="xCol">Output param</param>
-        public static void InterpolationStep(Image[][] dogPyr, int octave, int level, int x, int y,
+        public static void InterpolationStep(Image[][] dogPyramid, int octave, int level, int x, int y,
             out double xLevel, out double xRow, out double xCol)
         {
-            Vector<double> derivatives = Derivative3D(dogPyr, octave, level, x, y);
-            Matrix<double> invertedHessian = Hessian3D(dogPyr, octave, level, x, y).Inverse();
+            Vector<double> derivatives = Derivative3D(dogPyramid, octave, level, x, y);
+            Matrix<double> invertedHessian = Hessian3D(dogPyramid, octave, level, x, y).Inverse();
             Vector<double> result = invertedHessian.Multiply(-1).Multiply(derivatives);
 
             xLevel = result[2];
